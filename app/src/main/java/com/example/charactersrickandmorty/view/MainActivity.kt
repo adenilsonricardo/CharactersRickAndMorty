635f7140c961610261e1ab742d276463d12bd906
adenilson <adenilson.bernardes@bol.com.br>
2021-09-29 09:36:01 -0300

    defining mvvm architecture

--- app/src/main/java/com/example/charactersrickandmorty/view/MainActivity.kt
+++ app/src/main/java/com/example/charactersrickandmorty/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.charactersrickandmorty
+package com.example.charactersrickandmorty.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.charactersrickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
